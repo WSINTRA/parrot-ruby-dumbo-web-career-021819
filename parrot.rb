b45f87db4da8856c3e1f195fde95566b13fb0826
William Robert Simpson <will.simpson85@gmail.com>
2019-01-03 13:59:41 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(new_phrase="Squawk!")
+  outline = puts("new_phrase")
+  puts("new_phrase")
+  outline
+end
+  
